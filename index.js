ff47464573506a93c7e078d037b5983a29c6766f
S M Ibrahim Kayum <ibrahimkayum017@gmail.com>
2026-01-03 23:52:31 +0600

    Second commit

--- index.js
+++ index.js
@@ -57,7 +57,7 @@ async function run() {
       res.send(result);
     });
 
-    // Send a ping to confirm a successful connection
+    // Send a ping to confirm a successful connections
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
